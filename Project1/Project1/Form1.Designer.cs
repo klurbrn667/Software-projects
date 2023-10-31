3cf9ce3d272c917a0962977c6d8e2c14880826fb
klurbrn667 <149469629+klurbrn667@users.noreply.github.com>
2023-10-31 21:44:36 +0600

    Lab1

---
+++ Project1/Project1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(37, 53);
+            button1.Margin = new Padding(5, 5, 5, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(444, 86);
+            button1.TabIndex = 0;
+            button1.Text = "меняем свойтсво Visible";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.ForeColor = Color.Red;
+            button2.Location = new Point(37, 210);
+            button2.Margin = new Padding(5, 5, 5, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(444, 98);
+            button2.TabIndex = 1;
+            button2.Text = "меняем свойство Enabled";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.ForeColor = Color.Red;
+            button3.Location = new Point(37, 365);
+            button3.Margin = new Padding(5, 5, 5, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(444, 90);
+            button3.TabIndex = 2;
+            button3.Text = "меняем свойство Text и TextCanged";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(637, 96);
+            textBox1.Margin = new Padding(5, 5, 5, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(303, 39);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(734, 242);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 32);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1017, 828);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            ForeColor = SystemColors.HotTrack;
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "Hello World C#";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
