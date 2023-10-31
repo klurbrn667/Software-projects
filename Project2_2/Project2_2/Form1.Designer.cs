e51cf557672b6d9cda3c2447ff6565b62ae1fc2b
klurbrn667 <149469629+klurbrn667@users.noreply.github.com>
2023-10-31 21:46:14 +0600

    Lab 2

---
+++ Project2_2/Project2_2/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Project2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Location = new Point(49, 53);
+            button1.Margin = new Padding(5, 5, 5, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(676, 123);
+            button1.TabIndex = 0;
+            button1.Text = "MessageBox.Show(string)";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveBorder;
+            button2.Location = new Point(49, 211);
+            button2.Margin = new Padding(5, 5, 5, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(676, 123);
+            button2.TabIndex = 0;
+            button2.Text = "MessageBox.Show(string, string)";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveBorder;
+            button3.Location = new Point(49, 368);
+            button3.Margin = new Padding(5, 5, 5, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(676, 123);
+            button3.TabIndex = 0;
+            button3.Text = "MessageBox.Show(string, string, MessageBoxButtons)";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveBorder;
+            button4.Location = new Point(49, 528);
+            button4.Margin = new Padding(5, 5, 5, 5);
+            button4.Name = "button4";
+            button4.Size = new Size(676, 123);
+            button4.TabIndex = 0;
+            button4.Text = "MessageBox.Show(string, string, MessageBoxButtons, MessageBoxIcon)";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveBorder;
+            button5.Location = new Point(749, 211);
+            button5.Margin = new Padding(5, 5, 5, 5);
+            button5.Name = "button5";
+            button5.Size = new Size(514, 123);
+            button5.TabIndex = 0;
+            button5.Text = "Вызов диалового окна";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1300, 720);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
